dc072d0d1b80012bd704e3f8a6fca974f5e54cce
yerin719 <woow0719@gmail.com>
2025-08-10 16:42:11 +0900

    Navigation user part

--- app/root.tsx
+++ app/root.tsx
@@ -8,7 +8,7 @@ import {
 } from "react-router";
 
 import type { Route } from "./+types/root";
-import "./app.css";
+import stylesheet from "./app.css?url";
 import Navigation from "./common/components/navigation";
 
 export const links: Route.LinksFunction = () => [
@@ -22,11 +22,12 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  { rel: "stylesheet", href: stylesheet },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -45,7 +46,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
 export default function App() {
   return (
     <>
-      <Navigation />
+      <Navigation
+        isLoggedIn={true}
+        hasNotifications={true}
+        hasMessages={true}
+      />
       <Outlet />
     </>
   );
