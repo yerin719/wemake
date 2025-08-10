f5249cf943bc13f6e0342837b77e6bf70e0491ff
yerin719 <woow0719@gmail.com>
2025-08-10 15:27:45 +0900

    #1 Basics

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,3 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [index("common/pages/HelloWorld.tsx")] satisfies RouteConfig;
