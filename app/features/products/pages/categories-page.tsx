c4f84abb6e1eecb590737822bc5158de73eb4ea8
yerin719 <woow0719@gmail.com>
2025-08-13 14:06:15 +0900

    카테고리 카드 컴포넌트 추가 및 카테고리 페이지 수정

--- app/features/products/pages/categories-page.tsx
+++ app/features/products/pages/categories-page.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import { Hero } from "~/common/components/hero";
+import { CategoryCard } from "../components/category-card";
+import type { Route } from "./+types/categories-page";
+
+export const meta: Route.MetaFunction = () => [
+  { title: "Categories | ProductHunt Clone" },
+  { name: "description", content: "Browse products by category" },
+];
 
 export default function CategoriesPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Categories Page</h1>
+    <div className="space-y-10">
+      <Hero title="Categories" subtitle="Browse products by category" />
+      <div className="grid grid-cols-4 gap-10">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <CategoryCard
+            key={`categoryId-${index}`}
+            id={`categoryId-${index}`}
+            name="Category Name"
+            description="Category Description"
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- app/features/products/pages/category-page.tsx
+++ app/features/products/pages/category-page.tsx
@@ -1,9 +1,37 @@
-import React from "react";
+import { Hero } from "~/common/components/hero";
+import { ProductCard } from "../components/product-card";
+import ProductPagination from "~/common/components/product-pagination";
+import type { Route } from "./+types/category-page";
+
+export const meta = ({ params }: Route.MetaArgs) => {
+  return [
+    { title: `Developer Tools | ProductHunt Clone` },
+    { name: "description", content: `Browse Developer Tools products` },
+  ];
+};
 
 export default function CategoryPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Category Page</h1>
+    <div className="space-y-10">
+      <Hero
+        title={"Developer Tools"}
+        subtitle={`Tools for developers to build products faster`}
+      />
+
+      <div className="space-y-5 w-full max-w-screen-md mx-auto">
+        {Array.from({ length: 11 }).map((_, index) => (
+          <ProductCard
+            key={`productId-${index}`}
+            id={`productId-${index}`}
+            name="Product Name"
+            description="Product Description"
+            commentsCount={12}
+            viewsCount={12}
+            votesCount={120}
+          />
+        ))}
+      </div>
+      <ProductPagination totalPages={10} />
     </div>
   );
 }
