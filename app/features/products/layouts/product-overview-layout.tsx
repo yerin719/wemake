542c5ac37d7ed550d8aa2b631004d9e59dadb820
yerin719 <woow0719@gmail.com>
2025-08-13 17:35:58 +0900

    제품 개요 레이아웃에서 NavLink에 end 속성 추가

--- app/features/products/layouts/product-overview-layout.tsx
+++ app/features/products/layouts/product-overview-layout.tsx
@@ -39,6 +39,7 @@ export default function ProductOverviewLayout() {
       </div>
       <div className="flex gap-2.5">
         <NavLink
+          end
           className={({ isActive }) =>
             cn(
               buttonVariants({ variant: "outline" }),
