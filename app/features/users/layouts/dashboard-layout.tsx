40b9c2d95cc20711377b180c53804620882cb387
yerin719 <woow0719@gmail.com>
2025-08-17 16:22:09 +0900

    Dashboard Analytics

--- app/features/users/layouts/dashboard-layout.tsx
+++ app/features/users/layouts/dashboard-layout.tsx
@@ -58,7 +58,7 @@ export default function DashboardLayout() {
           </SidebarGroup>
         </SidebarContent>
       </Sidebar>
-      <div className=" h-full">
+      <div className="w-full h-full">
         <Outlet />
       </div>
     </SidebarProvider>
