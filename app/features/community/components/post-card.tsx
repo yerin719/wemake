d16168873a445f5186f31b1721a7a2e3cc5e03d2
yerin719 <woow0719@gmail.com>
2025-08-22 16:09:55 +0900

    Supabase Client - getTopics, getPosts

--- app/features/community/components/post-card.tsx
+++ app/features/community/components/post-card.tsx
@@ -21,7 +21,7 @@ interface PostCardProps {
   author: string;
   authorAvatarUrl: string | null;
   category: string;
-  postedAt: Date;
+  postedAt: string;
   expanded?: boolean;
   votesCount?: number;
 }
@@ -56,7 +56,7 @@ export function PostCard({
                 {author} on {category}
               </span>
               <DotIcon className="w-4 h-4" />
-              <span>{DateTime.fromJSDate(postedAt).toRelative()}</span>
+              <span>{DateTime.fromISO(postedAt).toRelative()}</span>
             </div>
           </div>
         </CardHeader>
--- app/features/community/pages/community-page.tsx
+++ app/features/community/pages/community-page.tsx
@@ -101,14 +101,14 @@ export default function CommunityPage({ loaderData }: Route.ComponentProps) {
           <div className="space-y-5">
             {loaderData.posts.map((post) => (
               <PostCard
-                key={post.id}
-                id={post.id}
+                key={post.post_id}
+                id={post.post_id}
                 title={post.title}
-                author={post.author}
-                authorAvatarUrl={post.authorAvatarUrl}
-                category={post.topic}
-                postedAt={post.createdAt}
-                votesCount={post.upvotes}
+                author={post.author.name}
+                authorAvatarUrl={post.author.avatar}
+                category={post.topic.name}
+                postedAt={post.created_at}
+                votesCount={post.upvotes[0].count}
                 expanded
               />
             ))}
